2a19f7e364cf02125b267b9cca7b854261954b33
Pallavi-Dongare <pallavidongare08@gmail.com>
2025-05-15 05:12:59 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from client import StockApi
+
+#create page title
+
+st.set_page_config("Stock market app")
+
+# add title
+
+st.title("stock market app")
+
+# add sub heading
+
+st.subheader("By Pallavi Dongare")
