4f2db216b52786f284e84699cc644e8235c151e3
Pallavi-Dongare <pallavidongare08@gmail.com>
2025-05-16 11:11:28 +0000

    updated

--- app.py
+++ app.py
@@ -12,3 +12,12 @@ st.title("stock market app")
 # add sub heading
 
 st.subheader("By Pallavi Dongare")
+
+# company = st.text_input("company name")
+
+# @st.cache_resource(ttl=3600)
+# def fetch_data():
+#     return StockApi()
+
+
+#create function for getting symbol
